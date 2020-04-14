756f13525afceb56602a1ccbf94be86a5742762c
Borbala Mester <mest0006@alognquinlive.com>
2020-04-14 11:42:31 -0400

    c

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
 window.onload = function () {
-
+  document.querySelector('.header').style.display = "block"
   document.getElementById('button').onclick = function () {
     document.getElementById('modal').style.display = "none"
     document.querySelector('.header').style.display = "block"
