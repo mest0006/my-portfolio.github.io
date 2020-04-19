e956e5bf3b29c27cf1fa7d7d04d42625200e98c1
Borbala Mester <mest0006@alognquinlive.com>
2020-04-19 12:09:52 -0400

    commit

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 
+let $skills = document.querySelector('.skillscontainer')
 
+let skillslist = ['Javascript', 'React', 'Vue', 'NodeJS', 'JQuery', 'Python', 'Ruby', 'Php', 'Wordpress', 'Drupal', 'Shopify', 'HTML', 'CSS']
+let empty = []
+
+for (let skills of skillslist) {
+  empty.push(` <li class=" skillslist"> ${skills} </li>`)
+}
+$skills.innerHTML = empty.join('');
 window.onload = function () {
-  document.querySelector('.header').style.display = "block"
+
   document.getElementById('button').onclick = function () {
+
     document.getElementById('modal').style.display = "none"
-    document.querySelector('.header').style.display = "block"
+
   };
 };
