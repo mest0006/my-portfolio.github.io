c999caa9c8baf252bb8ed9d851d14fc2a23b9ba8
Borbala Mester <mest0006@alognquinlive.com>
2020-04-26 22:38:52 -0400

    changed

--- index.js
+++ index.js
@@ -25,3 +25,11 @@ window.onload = function () {
 
   };
 };
+
+function myfunction () {
+  var copyText = document.getElementById("myInput")
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  alert("Copied the text: " + copyText.value);
+}
