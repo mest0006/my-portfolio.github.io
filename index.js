78044e59ad7c485a4383557fa1857d30bb4a7496
Borbala Mester <mest0006@alognquinlive.com>
2020-05-15 10:40:00 -0400

    update js

--- index.js
+++ index.js
@@ -16,7 +16,26 @@ let empty = []
 for (let skills of skillslist) {
   empty.push(` <li class=" skillslist"> ${skills} </li>`)
 }
+
 $skills.innerHTML = empty.join('');
+(function () {
+  if (window.localStorage) {
+    if (!localStorage.getItem('firstLoad')) {
+      localStorage['firstLoad'] = true;
+
+      document.getElementById('button').onclick = function () {
+
+        document.getElementById('modal').style.display = "none"
+
+      };
+
+      window.location.reload();
+    }
+    else
+      localStorage.removeItem('firstLoad');
+  }
+})();
+
 window.onload = function () {
 
   document.getElementById('button').onclick = function () {
